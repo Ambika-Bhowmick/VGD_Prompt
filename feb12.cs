d8f48ca3c887ba18ae46a2c21940b07e4d4a51f5
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-02-12 09:31:58 -0700

    Create feb12.cs

---
+++ feb12.cs
@@ -0,0 +1,11 @@
+//This is my code for February 12, 2025
+Console.WriteLine('b');
+Console.WriteLine("Hello World!");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625F);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+//I liked how it told us which letter/chracter gave us a string, interger, float, or decimal.
+//I didn't like how it made us repeat things and read so much.
