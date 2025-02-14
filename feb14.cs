5704744b8ef5fc68f4f5d198753a4b004fac6eae
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-02-14 10:02:19 -0700

    Create feb14.cs

---
+++ feb14.cs
@@ -0,0 +1,7 @@
+// We learned how to store variables. I learned that you have to type to variable type before storing. This is not the same as Python, as you use the equal sign
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+// We did not learn how to use the stored variables yet.
