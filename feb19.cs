f1715246dace0279d06fabf94ee9de2bc192a693
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-02-19 08:39:39 -0700

    Create feb19.cs

---
+++ feb19.cs
@@ -0,0 +1,10 @@
+// We learnt how to store and use variables. We did this by making a code that stores the variable that is call "firstName".
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
