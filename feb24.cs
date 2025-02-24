e5ea79e8ab77a504b45f34a32c8381a4de1cfb6e
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-02-24 13:53:57 -0700

    Create feb24.cs

---
+++ feb24.cs
@@ -0,0 +1,11 @@
+// We learned how to use variables and create a sentence with them. We did this by storing the variables and then using them in a sentence by using "Console.Write".
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
