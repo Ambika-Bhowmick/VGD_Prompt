ee787977b8d833ca094198562c93ffe084047d25
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-03-05 09:03:14 -0700

    Create mar05.cs

---
+++ mar05.cs
@@ -0,0 +1,12 @@
+// I learned how to create variables. I also figured out what floats, decimals are.
+Console.WriteLine('b');
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
