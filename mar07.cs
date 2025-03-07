c967d1bd007def7b1aa73b5810530f5e5712b991
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-03-07 10:10:53 -0700

    Create mar07.cs

---
+++ mar07.cs
@@ -0,0 +1,15 @@
+// I learned how to store variables and combine them with stings.
+string firstname;
+firstname = "Bob";
+int numBer;
+numBer = 3;
+decimal temperature;
+temperature = 34.4m;
+Console.Write("Hello, ");
+Console.Write(firstname);
+Console.Write("! You have ");
+Console.Write(numBer);
+Console.Write("messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius. ");
+// I got 100% on the knowledge check.
