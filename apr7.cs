2c2fc3162b7bb6e3676cbdc29382c613233e45f5
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-04-07 13:47:39 -0600

    Create apr7.cs

---
+++ apr7.cs
@@ -0,0 +1,9 @@
+// Mod. 5 Unit 1 & 2
+// We learned how to add basic numbers by using the adding parameters.
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
