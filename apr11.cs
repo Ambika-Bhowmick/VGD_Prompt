217ccb3ac0ace0f19e59885774283664beb9dfe0
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-04-11 10:00:51 -0600

    Create apr11.cs

---
+++ apr11.cs
@@ -0,0 +1,5 @@
+// Mod. 5 - Unit 5 & 6
+// I learned how to use my knowledge of operations in C# to convert fahrenheit to celsius.
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " +  celsius + " Celcius.") ;
