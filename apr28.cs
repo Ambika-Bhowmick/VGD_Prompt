56cfa1c77db372aa2909d9e7eb38523bf45f81d3
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-04-28 13:55:03 -0600

    Create apr28.cs

---
+++ apr28.cs
@@ -0,0 +1,25 @@
+// We learned how to use operations and do compound assignments.
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+int value = 1;
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value
